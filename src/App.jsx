dcf1d9411b13497af36a5a49c07bce5bbea0b693
Richard <richardmusime6@gmail.com>
2024-07-28 17:38:46 +0200

    Added a window resizer app using react js

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import WindowTracker from './components/WindowTracker'
+
+function App() {
+
+
+  return (
+    <>
+      <WindowTracker/>
+    </>
+  )
+}
+
+export default App
